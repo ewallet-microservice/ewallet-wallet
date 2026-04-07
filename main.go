08375408e6a35c149f2eaf5818c4e040051ee35f
Muhammad Hasnan Regard <muhammad.regard@shopee.com>
2026-04-08 06:07:00 +0700

    feat: initialize wallet service

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"github.com/mhasnanr/ewallet-wallet/bootstrap"
+	"github.com/mhasnanr/ewallet-wallet/cmd"
+)
+
+func main() {
+	if err := bootstrap.SetupZapLogger(); err != nil {
+		log.Fatal("failed to initialize logger")
+	}
+
+	if err := bootstrap.SetupConfig(".env"); err != nil {
+		log.Fatalf("failed to load config file")
+	}
+
+	bootstrap.SetupDatabase()
+
+	go cmd.ServeGRPC()
+	cmd.ServeHTTP()
+}
