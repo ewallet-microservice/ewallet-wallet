c0c74371a83f8ded7173b484a7ae0e68afd0f7e5
Muhammad Hasnan Regard <muhammad.regard@shopee.com>
2026-05-03 16:15:49 +0700

    feat: implement wallet transaction server

---
+++ internal/handler/grpc/wallettransaction.go
@@ -0,0 +1,71 @@
+package grpc
+
+import (
+	"context"
+
+	pb "github.com/mhasnanr/ewallet-wallet/cmd/walletTransaction"
+	"github.com/mhasnanr/ewallet-wallet/constants"
+	"github.com/mhasnanr/ewallet-wallet/internal/models"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type WalletTransactionService interface {
+	DebitBalance(ctx context.Context, userID int, req models.TransactionRequest) (models.BalanceResponse, error)
+	CreditBalance(ctx context.Context, userID int, req models.TransactionRequest) (models.BalanceResponse, error)
+}
+
+type WalletTransactionSystem struct {
+	pb.UnimplementedWalletTransactionServer
+	svc WalletTransactionService
+}
+
+func NewWalletTransactionSystem(walletSvc WalletTransactionService) *WalletTransactionSystem {
+	return &WalletTransactionSystem{svc: walletSvc}
+}
+
+func (e *WalletTransactionSystem) DebitBalance(ctx context.Context, request *pb.WalletRequest) (*pb.WalletResponse, error) {
+	userID := request.GetUserID()
+	if userID == 0 {
+		return nil, status.Error(codes.InvalidArgument, "user id is required")
+	}
+
+	walletRequest := models.TransactionRequest{
+		Reference: request.GetReference(),
+		Amount:    request.GetAmount(),
+	}
+	wallet, err := e.svc.DebitBalance(ctx, int(request.GetUserID()), walletRequest)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "internal server error: %v", err)
+	}
+
+	return &pb.WalletResponse{
+		Message: constants.DebitBalance,
+		Data: &pb.WalletData{
+			Balance: wallet.Balance,
+		},
+	}, nil
+}
+
+func (e *WalletTransactionSystem) CreditBalance(ctx context.Context, request *pb.WalletRequest) (*pb.WalletResponse, error) {
+	userID := request.GetUserID()
+	if userID == 0 {
+		return nil, status.Error(codes.InvalidArgument, "user id is required")
+	}
+
+	walletRequest := models.TransactionRequest{
+		Reference: request.GetReference(),
+		Amount:    request.GetAmount(),
+	}
+	wallet, err := e.svc.CreditBalance(ctx, int(request.GetUserID()), walletRequest)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "internal server error: %v", err)
+	}
+
+	return &pb.WalletResponse{
+		Message: constants.CreditBalance,
+		Data: &pb.WalletData{
+			Balance: wallet.Balance,
+		},
+	}, nil
+}
